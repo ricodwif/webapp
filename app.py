d95799450d941a88d526992903e06b193daf17b5
ricodwif <130067006+ricodwif@users.noreply.github.com>
2023-11-30 08:06:59 +0000

    update app

--- app.py
+++ app.py
@@ -1,79 +1,75 @@
 import streamlit as st
 from sqlalchemy import text
 
-list_maskapai = ['', 'Garuda Indonesia', 'Lion Air', 'Citilink', 'Batik Air', 'Sriwijaya Air', 'NAM Air', 'AirAsia Indonesia', 
-                 'Wings Air', 'TransNusa', 'Indonesia AirAsia X', 'Susi Air', 'Kalstar Aviation', 'Trigana Air', 'Pelita Air', 
-                 'Aviastar Mandiri']
-list_bandara = ['', 'Soekarno-Hatta', 'Ngurah Rai', 'Juanda', 'Sultan Hasanuddin', 'Minangkabau International', 'Adisutjipto', 
-                 'Kuala Namu International', 'Sultan Aji Muhammad Sulaiman', 'Sultan Syarif Kasim II', 'El Tari', 'Wamena', 
-                 'Sentani International', 'Supadio International', 'Sultan Syarif Qasim II', 'Juwata International', 
-                 'Raja Haji Fisabilillah International', 'Frans Kaisiepo International', 'Bali International', 'Sultan Iskandar Muda']
-
-list_gate = ['', 'Gate A1', 'Gate B2', 'Gate C3', 'Gate D4', 'Gate E5', 'Gate F6', 'Gate G7', 'Gate H8', 'Gate I9', 'Gate J10',
-              'Gate K11', 'Gate L12', 'Gate M13', 'Gate N14', 'Gate O15', 'Gate P16', 'Gate Q17', 'Gate R18', 'Gate S19', 
-              'Gate T20', 'Gate U21', 'Gate V22', 'Gate W23', 'Gate X24']
-
-list_status = ['', 'On Time', 'Delayed']
-
-list_layanan_pesawat = ['', 'Ekonomi', 'Bisnis', 'First Class']
+maskapai = ['', 'Garuda Indonesia', 'Lion Air', 'Citilink', 'Batik Air', 'Sriwijaya Air', 'NAM Air', 'AirAsia Indonesia', 'Wings Air', 'TransNusa', 'Susi Air']
+status_penerbangan = ['', 'On time', 'Delay', 'Last call']
 
 conn = st.connection("postgresql", type="sql", 
                      url="postgresql://dwirico08:6wgkTJSMoL1U@ep-curly-salad-29186979.us-east-2.aws.neon.tech/web")
 with conn.session as session:
-    query = text('DROP TABLE IF EXISTS schedule;'
-                 'CREATE TABLE schedule (id serial, maskapai text, bandara_asal text, bandara_tujuan text,'
-                 'waktu_keberangkatan time, waktu_sampai time, tanggal date, gate_keberangkatan text,'
-                 'status_penerbangan text, Layanan_pesawat text, max_capacity integer);')
+    query = text('CREATE TABLE IF NOT EXISTS SCHEDULE (id serial, maskapai varchar, bandara_asal varchar, bandara_tujuan text, \
+                                                       waktu_keberangkatan time, waktu_sampai time, tanggal date, gate_keberangkatan text, status_penerbangan text, layanan_pesawat text, max_capacity varchar);')
     session.execute(query)
 
-st.header('AIRLINE FLIGHT SCHEDULE MANAGEMENT SYSTEM')
-page = st.sidebar.selectbox("Choose Menu", ["View Data", "Edit Data"])
+st.header('AIRPORT DATA MANAGEMENT SYS')
+page = st.sidebar.selectbox("Pilih Menu", ["View Data","Edit Data"])
 
 if page == "View Data":
-    data = conn.query('SELECT * FROM schedule ORDER BY id;', ttl="0").set_index('id')
+    data = conn.query('SELECT * FROM schedule ORDER By id;', ttl="0").set_index('id')
     st.dataframe(data)
 
 if page == "Edit Data":
-    if st.button('Add Data'):
+    if st.button('Tambah Data'):
         with conn.session as session:
-            query = text('INSERT INTO schedule (maskapai, bandara_asal, bandara_tujuan, tanggal, waktu_keberangkatan,'
-                         'waktu_sampai, gate_keberangkatan, status_penerbangan, Layanan_pesawat, max_capacity) VALUES'
-                         '(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10);')
-            session.execute(query, {'1': '', '2': '', '3': '', '4': None, '5': None, '6': None, '7': '', '8': '', '9': '[]', '10': None})
+            query = text('INSERT INTO schedule (maskapai, bandara_asal, bandara_tujuan, waktu_keberangkatan, waktu_sampai, tanggal, gate_keberangkatan, status_penerbangan, layanan_pesawat, max_capacity) \
+                          VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10);')
+            session.execute(query, {'1':'', '2':'', '3':'', '4':None, '5':None, '6':None, '7':'[]', '8':'[]', '9':'[]', '10':''})
             session.commit()
 
-    data = conn.query('SELECT * FROM schedule ORDER BY id;', ttl="0")
-    for _, result in data.iterrows():
+    data = conn.query('SELECT * FROM schedule ORDER By id;', ttl="0")
+    for _, result in data.iterrows():        
         id = result['id']
         maskapai_lama = result["maskapai"]
         bandara_asal_lama = result["bandara_asal"]
         bandara_tujuan_lama = result["bandara_tujuan"]
-        tanggal_lama = result["tanggal"]
         waktu_keberangkatan_lama = result["waktu_keberangkatan"]
         waktu_sampai_lama = result["waktu_sampai"]
+        tanggal_lama = result["tanggal"]
         gate_keberangkatan_lama = result["gate_keberangkatan"]
         status_penerbangan_lama = result["status_penerbangan"]
-        layanan_pesawat_lama = result["Layanan_pesawat"]
+        layanan_pesawat_lama = result["layanan_pesawat"]
         max_capacity_lama = result["max_capacity"]
 
-        with st.expander(f'{maskapai_lama} - {bandara_asal_lama} to {bandara_tujuan_lama}'):
+        with st.expander(f'a.n. {maskapai_lama}'):
             with st.form(f'data-{id}'):
-                maskapai_baru = st.selectbox("Airline", list_maskapai, list_maskapai.index(maskapai_lama))
-                bandara_asal_baru = st.selectbox("Departure Airport", list_bandara, list_bandara.index(bandara_asal_lama))
-                bandara_tujuan_baru = st.selectbox("Destination Airport", list_bandara, list_bandara.index(bandara_tujuan_lama))
-                tanggal_baru = st.date_input("Date", tanggal_lama)
-                waktu_keberangkatan_baru = st.time_input("Departure Time", waktu_keberangkatan_lama)
-                waktu_sampai_baru = st.time_input("Arrival Time", waktu_sampai_lama)
-                gate_keberangkatan_baru = st.selectbox("Departure Gate", list_gate, list_gate.index(gate_keberangkatan_lama))
-                status_penerbangan_baru = st.selectbox("Flight Status", list_status, list_status.index(status_penerbangan_lama))
-                layanan_pesawat_baru = st.multiselect("Services", list_layanan_pesawat, eval(layanan_pesawat_lama))
-                max_capacity_baru = st.number_input("Max Capacity", max_capacity_lama)
+                bandara_asal_baru = st.text_input("bandara_asal", bandara_asal)
+                maskapai_baru = st.selectbox("maskapai_name",list_maskapai, list_maskapai.index(maskapai_lama))
+                bandara_tujuan_baru = st.text_input("bandara_tujuan", bandara_tujuan)
+                waktu_keberangkatan_baru = st.time_input("waktu_keberangkatan", waktu_keberangkatan)
+                waktu_sampai_baru = st.time_input("waktu_sampai", waktu_sampai)
+                tanggal_baru = st.date_input("tanggal", tanggal_lama)
+                gate_keberangkatan_baru = st.text_input("gate_keberangkatan", gate_keberangkatan)
+                status_penerbangan_baru = st.selectbox("status_penerbangan", list_status_penerbangan, list_status_penerbangan.index(status_penerbangan_lama))
+                layanan_pesawat_baru = st.multiselect("layanan_pesawat", ['Ekonomi', 'Bisnis', 'First Class'], eval(layanan_pesawat_lama))
+                max_capacity_baru = st.text_input("max_capacity", max_capacity_lama)
+                
+                col1, col2 = st.columns([1, 7])
 
-                if st.form_submit_button('UPDATE'):
-                    with conn.session as session:
-                        query = text('UPDATE schedule SET maskapai=:1, bandara_asal=:2, bandara_tujuan=:3,'
-                                     'tanggal=:4, waktu_keberangkatan=:5, waktu_sampai=:6, gate_keberangkatan=:7,'
-                                     'status_penerbangan=:8, Layanan_pesawat=:9, max_capacity=:10 WHERE id=:11;')
-                        session.execute(query, {'1': maskapai_baru, '2': bandara_asal_baru, '3': bandara_tujuan_baru,
-                                               '4': tanggal_baru, '5': waktu_keberangkatan_baru, '6': waktu_sampai_baru,
-                                               '7': gate_keberangkatan_baru, '8': status_penerbangan_baru,
+                with col1:
+                    if st.form_submit_button('UPDATE'):
+                        with conn.session as session:
+                            query = text('UPDATE schedule \
+                                          SET maskapai=:1, bandara_asal=:2, bandara_tujuan=:3, waktu_keberangkatan=:4, \
+                                          waktu_sampai=:5, tanggal=:6, gate_keberangkatan=:7, status_penerbangan=:8, layanan_pesawat=:9, max_capacity=:10\
+                                          WHERE id=:11;')
+                            session.execute(query, {'1':maskapai_baru, '2':bandara_asal_baru, '3':bandara_tujuan_baru, '4':waktu_keberangkatan_baru, 
+                                                    '5':waktu_sampai_baru, '6':tanggal_baru, '7':gate_keberangkatan_baru, '8':status_penerbangan_baru, '9':str(layanan_pesawat_baru), '10':max_capacity_baru, '11':id})
+                            session.commit()
+                            st.experimental_rerun()
+                
+                with col2:
+                    if st.form_submit_button('DELETE'):
+                        query = text(f'DELETE FROM schedule WHERE id=:1;')
+                        session.execute(query, {'1':id})
+                        session.commit()
+                        st.experimental_rerun()
--- app.sql
+++ app.sql
@@ -1,26 +1,28 @@
-drop table if exists schedule;
-create table schedule (
-	id serial,
-	doctor_name text,
-	patient_name text,
-	gender text,
-	symptom text,
-	handphone text,
-	address text,
-	waktu time,
-	tanggal date
-);
-
-insert into schedule (doctor_name, patient_name, gender, symptom, handphone, address, waktu, tanggal) 
+DROP TABLE IF EXISTS schedule;
+CREATE TABLE schedule (
+    id serial,
+    maskapai text,
+    bandara_asal text,
+    bandara_tujuan text,
+    waktu_keberangkatan time,
+    waktu_sampai time,
+    tanggal date,
+    gate_keberangkatan text,
+    status_penerbangan text,
+    Layanan_pesawat text,
+    max_capacity integer);
+insert into schedule (	maskapai, bandara_asal,bandara_tujuan,tanggal, waktu_keberangkatan, waktu_sampai, gate_keberangkatan,
+    status_penerbangan,Layanan_pesawat,max_capacity)
 values
-	('dr. Nurita', 'Ahmad Maulana', 'male', '["headache", "stomache"]', 62838, 'address1', '08:00', '2023-10-01'),
-	('dr. Yogi', 'Renata Zahab', 'female', '["cough", "flu"]', 62838, 'address2', '09:00', '2022-10-02'),
-	('dr. Wibowo', 'Nunuk Reni', 'female', '["cough", "flu"]', 62838, 'address3', '10:00', '2022-10-03'),
-	('dr. Yogi', 'Bro Ulil', 'male', '["cough", "headache"]', 62838, 'address4', '11:00', '2022-10-04'),
-	('dr. Ulama', 'Wah Bowi', 'male', '["headache", "flu"]', 62838, 'address5', '12:00', '2022-10-05'),
-	('dr. Ulama', 'Iis Mika', 'female', '["cough", "flu", "stomache", "headache"]', 62838, 'address6', '08:00', '2022-10-06'),
-	('dr. Ping', 'Zizah Lana', 'female', '["flu", "stomache", "headache"]', 62838, 'address7', '09:00', '2022-10-07'),
-	('dr. Nurita', 'Alif Iman', 'male', '["cough", "flu", "headache"]', 62838, 'address8', '10:00', '2022-10-08'),
-	('dr. Ping', 'Zaka Zaki', 'female', '["cough", "stomache", "headache"]', 62838, 'address9', '11:00', '2022-10-09'),
-	('dr. Wibowo', 'Faus Rahmi', 'male', '["cough"]', 62838, 'address10', '12:00', '2022-10-11')
-	;
+	('Garuda Indonesia', 'Soekarno-Hatta', 'Ngurah Rai', '2023-11-22', '08:30', '10:30', 'Gate A1', 'On Time', '["Ekonomi", "Bisnis", "First Class"]', 300),
+    ('Lion Air', 'Juanda', 'Sultan Hasanuddin', '2023-11-22', '09:15', '11:30', 'Gate B2', 'Delayed', '["Ekonomi", "Bisnis", "First Class"]', 250),
+    ('Citilink', 'Ngurah Rai', 'Minangkabau International', '2023-11-22', '10:00', '12:00', 'Gate C3', 'On Time', '["Ekonomi", "Bisnis", "First Class"]', 200),
+    ('Batik Air', 'Adisutjipto', 'Kuala Namu International', '2023-11-22', '11:45', '14:00', 'Gate D4', 'Delay','["Ekonomi", "Bisnis", "First Class"]', 280),
+    ('Sriwijaya Air', 'Minangkabau International', 'Adisutjipto', '2023-11-22', '12:30', '14:45', 'Gate E5', 'On Time', '["Ekonomi", "Bisnis", "First Class"]', 230),
+    ('NAM Air', 'Sultan Hasanuddin', 'Ngurah Rai', '2023-11-22', '13:15', '15:30', 'Gate F6', 'Last Call', '["Ekonomi", "Bisnis", "First Class"]', 260),
+    ('AirAsia Indonesia', 'Ngurah Rai', 'Juanda', '2023-11-22', '14:00', '16:00', 'Gate G7', 'Delayed', '["Ekonomi", "Bisnis", "First Class"]', 220),
+    ('Wings Air', 'Sultan Aji Muhammad Sulaiman', 'Sultan Hasanuddin', '2023-11-22', '15:45', '18:00', 'Gate H8', 'On Time','["Ekonomi", "Bisnis", "First Class"]', 180),
+    ('TransNusa', 'Adisutjipto', 'Sultan Syarif Kasim II', '2023-11-22', '16:30', '18:45', 'Gate I9', 'On Time', '["Ekonomi", "Bisnis", "First Class"]', 150),
+    ('Susi Air', 'Sultan Hasanuddin', 'Sentani International', '2023-11-23', '07:00', '10:15', 'Gate X24', 'On Time', '["Ekonomi", "Bisnis", "First Class"]', 50);
+
+SELECT * FROM schedule;
